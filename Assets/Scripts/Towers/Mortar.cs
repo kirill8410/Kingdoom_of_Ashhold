798cf606c9e6d31401b2948ea47000ee509cddbd
Mr Mystic <160300164+kirill8410@users.noreply.github.com>
2024-12-16 17:27:56 +0700

    a

--- Assets/Scripts/Towers/Mortar.cs
+++ Assets/Scripts/Towers/Mortar.cs
@@ -1,16 +1,123 @@
 using UnityEngine;
+using System.Collections;
+using NUnit.Framework.Constraints;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.InputSystem.Switch;
 
-public class Mortar : MonoBehaviour
+public class Mortar : Tower, TowerFunctions
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Level")]
+
+    public int PriceLevelUp { get; set; }
+    public int Towerlevel { get; set; } = 1;
+
+    [SerializeField] LevelUp _levelUp;
+    public LevelUp levelUp
     {
-        
+        get
+        {
+            return _levelUp;
+        }
+        set
+        {
+            _levelUp = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("MortarType")]
+
+    [SerializeField] MortarType mortarType;
+    private enum MortarType
     {
-        
+        Simple
+    }
+
+    private int trueDamage;
+
+    public Enemy target;
+
+    private void Start()
+    {
+        StartCoroutine(SearchTarget());
+        StartCoroutine(Attack());
+    }
+    private void Update()
+    {
+        Distance.transform.localScale = new Vector3(attackDistance * 2f, attackDistance * 2f, 1f);
+        if (Towerlevel == 1)
+        {
+            PriceLevelUp = levelUp.priceLevelUp_1;
+        }
+        else if (Towerlevel == 2)
+        {
+            PriceLevelUp = levelUp.priceLevelUp_2;
+        }
+    }
+
+    public IEnumerator Attack()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f / attackSpeed);
+            if ((target != null) && (isAttack) && Vector3.Distance(gameObject.transform.position,
+                target.gameObject.transform.position) <= attackDistance)
+            {
+                trueDamage = damage;
+                if (damageType == target.protectionType)
+                {
+                    trueDamage -= target.protection;
+                }
+                if (trueDamage < 0)
+                {
+                    trueDamage = 0;
+                }
+
+                switch (mortarType)
+                {
+                    case MortarType.Simple:
+                        isAttack = false;
+                        GameObject attack = Instantiate(attackPrefab, attackPoint.position, attackPoint.rotation);
+                        break;
+                }
+            }
+        }
+    }
+    public IEnumerator SearchTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (target != null && Vector3.Distance(target.transform.position, gameObject.transform.position) > attackDistance)
+            {
+                target = null;
+            }
+            Enemy[] enemyes = Object.FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+            foreach (Enemy enemy in enemyes)
+            {
+                if (Vector3.Distance(gameObject.transform.position, enemy.transform.position) <= attackDistance)
+                {
+                    if ((target == null) || (enemy.numberPoint > target.numberPoint) ||
+                        ((enemy.distanceToPoint < target.distanceToPoint) && (enemy.numberPoint >= target.numberPoint)))
+                    {
+                        target = enemy;
+                    }
+                }
+            }
+        }
+    }
+    public void LevelUp()
+    {
+        if (Towerlevel == 1)
+        {
+            damage += levelUp.damage_1;
+            attackDistance += levelUp.distance_1;
+            Towerlevel = 2;
+        }
+        else if (Towerlevel == 2)
+        {
+            damage += levelUp.damage_2;
+            attackDistance += levelUp.distance_2;
+            Towerlevel = 3;
+        }
     }
 }
